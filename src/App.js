fde768e71179da2d654dab169702e2da0f61c0f5
SamVJulius <samsonvjulius@gmail.com>
2023-12-26 12:40:25 +0530

    Contact Page updated

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import './App.css';
 import Navbar from './pages/homepage/components/navbar/Navbar';
 import Landing from './pages/homepage/components/landing/Landing';
 import About from './pages/homepage/components/About/About';
+import ContactUs from './pages/homepage/components/ContactUs/ContactUs';
 import Footer from './pages/homepage/components/Footer/Footer';
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Landing />
-      <About />
-      <Footer />
+      {/* <Navbar /> */}
+      {/* <Landing /> */}
+      {/* <About /> */}
+      <ContactUs/>
+      {/* <Footer /> */}
     </div>
 
   );
