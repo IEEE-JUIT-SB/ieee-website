96926664b9e9d82437441812f5bd081c677492c6
Mayank Kumar <mayank.k9802@gmail.com>
2023-12-27 14:48:50 +0530

    Footer

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import './App.css';
-import Navbar from './pages/homepage/components/navbar/Navbar';
-import Landing from './pages/homepage/components/landing/Landing';
-import About from './pages/homepage/components/About/About';
-import ContactUs from './pages/homepage/components/ContactUs/ContactUs';
-import Footer from "./footer/footer";
-
+import "./App.css";
+import Navbar from "./pages/homepage/components/navbar/Navbar";
+import Landing from "./pages/homepage/components/landing/Landing";
+import About from "./pages/homepage/components/About/About";
+import ContactUs from "./pages/homepage/components/ContactUs/ContactUs";
+import Footer from "./pages/homepage/components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       {/* <Navbar /> */}
       {/* <Landing /> */}
-// <<<<<<< main
+      {/* // <<<<<<< main */}
       <About />
       <ContactUs />
       {/* <Footer /> */}
-=======
+      =======
       {/* <About /> */}
-      <ContactUs/>
+      <ContactUs />
       <Footer />
-// >>>>>>> main
+      {/* // >>>>>>> main */}
     </div>
-
   );
 }
 
