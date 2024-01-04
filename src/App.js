38f14ca0e76a46f5694d5c241a2bd997ed6a6f18
Mayank Kumar <mayank.k9802@gmail.com>
2024-01-04 21:57:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Landing />
-      <About />
+      {/* <About /> */}
       <UpcomingEvents />
       <Testimonials />
       <ContactUs />
