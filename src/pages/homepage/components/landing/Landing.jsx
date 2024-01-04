25bae800e63627e50337e442614183c84a28075e
Mayank Kumar <mayank.k9802@gmail.com>
2024-01-05 00:10:21 +0530

    404 Page

--- src/pages/homepage/components/landing/Landing.jsx
+++ src/pages/homepage/components/landing/Landing.jsx
@@ -1,36 +1,36 @@
-import React from 'react';
-import './Landing.css';
-import landing_bg from './Assets/Landing_bg.png';
-import box from './Assets/Rectangle3.png';
-import IEEE from './Assets/IEEE.png';
-import middleText from './Assets/DREAM. DESIGN. DEVELOP..png';
-import img1 from './Assets/img1.png';
-import img2 from './Assets/img2.png';
-import img3 from './Assets/img3.png';
+import React from "react";
+import "./Landing.css";
+import landing_bg from "./Assets/Landing_bg.png";
+import box from "./Assets/Rectangle3.png";
+import IEEE from "./Assets/IEEE.png";
+import middleText from "./Assets/DREAM. DESIGN. DEVELOP..png";
+import img1 from "./Assets/img1.png";
+import img2 from "./Assets/img2.png";
+import img3 from "./Assets/img3.png";
 
 const backgroundStyle = {
-  width: '100%', // Set the image width to 100% of the container
-  height: '100vh', // Set the image height to 100% of the viewport height
-  objectFit: 'cover', // Make sure the image covers the container without distortion
+  width: "100%", // Set the image width to 100% of the container
+  height: "100vh", // Set the image height to 100% of the viewport height
+  objectFit: "cover", // Make sure the image covers the container without distortion
 };
 
 export default function Landing() {
   return (
     <div className="background-container">
       <img src={landing_bg} alt="" style={backgroundStyle} />
-      <div className='IEEE'>
+      <div className="IEEE">
         <img src={IEEE} alt="" />
       </div>
-      <div className='text'>
+      <div className="text">
         <img src={middleText} alt="" />
       </div>
-      <div className='box'>
+      <div className="box">
         <img src={box} alt="" />
       </div>
-      <div className='images'>
-          <img src={img1} alt="" />
-          <img src={img2} alt="" />
-          <img src={img3} alt="" />
+      <div className="images">
+        <img src={img1} alt="" />
+        <img src={img2} alt="" />
+        <img src={img3} alt="" />
       </div>
     </div>
   );
---
+++ src/pages/noPage/NoPage.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Navbar from "../homepage/components/navbar/Navbar";
+import "./NoPage.css";
+import Footer from "../homepage/components/footer/Footer";
+import Asset from "./assets/asset.png";
+
+export default function NoPage() {
+  return (
+    <div className="no-page">
+      <Navbar />
+      <div className="no-page-in">
+        <div className="no-page-in-1">
+          <h1>Error 404!!! Page not Found !!!</h1>
+          <h2>Sorry, we couldn't find the page you were looking for.</h2>
+        </div>
+        <img src={Asset} alt="" id="asset-1" />
+        <img src={Asset} alt="" id="asset-2" />
+        <img src={Asset} alt="" id="asset-3" />
+        <img src={Asset} alt="" id="asset-4" />
+        {/* <img src={Asset} alt="" id="asset-5" />
+        <img src={Asset} alt="" id="asset-6" />
+        <img src={Asset} alt="" id="asset-7" />
+        <img src={Asset} alt="" id="asset-8" />
+        <img src={Asset} alt="" id="asset-9" />
+        <img src={Asset} alt="" id="asset-10" /> */}
+        <Footer />
+      </div>
+    </div>
+  );
+}
