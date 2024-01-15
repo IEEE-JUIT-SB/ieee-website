cbba039fdf74605b1b39dad261d38c5410408804
Mayank Kumar <mayank.k9802@gmail.com>
2024-01-15 23:02:15 +0530

    Landing Updates

--- src/pages/homepage/components/landing/Landing.jsx
+++ src/pages/homepage/components/landing/Landing.jsx
@@ -1,37 +1,44 @@
 import React from "react";
 import "./Landing.css";
-import landing_bg from "./Assets/Landing_bg.png";
 import box from "./Assets/Rectangle3.png";
 import IEEE from "./Assets/IEEE.png";
 import middleText from "./Assets/DREAM. DESIGN. DEVELOP..png";
 import img1 from "./Assets/img1.png";
 import img2 from "./Assets/img2.png";
 import img3 from "./Assets/img3.png";
-
-const backgroundStyle = {
-  width: "100%", // Set the image width to 100% of the container
-  height: "100vh", // Set the image height to 100% of the viewport height
-  objectFit: "cover", // Make sure the image covers the container without distortion
-};
+import "./LandingBG.css";
+import Starfield from "./Starfield";
+import Asset2 from "./Assets/asset2.png";
 
 export default function Landing() {
   return (
     <div className="background-container" id="home">
-      <img src={landing_bg} alt="" style={backgroundStyle} />
-      <div className="IEEE">
-        <img src={IEEE} alt="" />
+      {/* <div class="bg"></div> */}
+      <img src={Asset2} alt="" id="bg-asset" />
+
+      {/* <div class="star-field">
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+      </div> */}
+      <div className="starfield">
+        <Starfield />
       </div>
-      <div className="text">
-        <img src={middleText} alt="" />
+      {/* <canvas id="particle"></canvas> */}
+      <div id="space-warp">
+        <div class="stars"></div>
+        <div class="stars"></div>
+        <div class="stars"></div>
+        <div class="stars"></div>
+        <div class="stars"></div>
       </div>
-      <div className="box">
-        <img src={box} alt="" />
+
+      <div className="landing-section1">
+        <h1 id="ieee-landing">IEEE</h1>
+        <h2 id="ieee-quote">DREAM . DESIGN . DEVELOP</h2>
       </div>
-      <div className="images">
-        <img src={img1} alt="" />
-        <img src={img2} alt="" />
-        <img src={img3} alt="" />
-      </div>
+      <div className="landing-section2"></div>
     </div>
   );
 }
---
+++ src/pages/homepage/components/landing/Starfield.jsx
@@ -0,0 +1,115 @@
+import React, { useEffect, useRef } from "react";
+
+const Starfield = () => {
+  let canvasRef = useRef(null);
+  let ctx;
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    ctx = canvas.getContext("2d");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    // Global Animation Setting
+    window.requestAnimFrame =
+      window.requestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      window.oRequestAnimationFrame ||
+      window.msRequestAnimationFrame ||
+      function (callback) {
+        window.setTimeout(callback, 1000000 / 60);
+      };
+
+    // Particles Around the Parent
+    function Particle(x, y, distance) {
+      this.angle = Math.random() * 5 * Math.PI;
+      this.radius = Math.random();
+      this.opacity = (Math.random() * 10 + 2) / 10;
+      this.distance = (1 / this.opacity) * distance;
+
+      this.speed = ((Math.floor(Math.random() * 2.5) + 0.5) * Math.PI) / 360; // The rate at which this star will orbit
+      this.rotation = 1000; // current Rotation
+      this.startRotation =
+        ((Math.floor(Math.random() * 360) + 0.5) * Math.PI) / 180; // Starting rotation.  If not random, all stars will be generated in a single line.
+
+      this.draw = function () {
+        ctx.fillStyle = "rgba(255,255,255," + this.opacity + ")";
+        ctx.beginPath();
+        ctx.arc(
+          this.position.x,
+          this.position.y,
+          this.radius,
+          0,
+          Math.PI * 2,
+          false
+        );
+        ctx.fill();
+        ctx.closePath();
+      };
+      this.update = function () {
+        this.angle += this.speed;
+        this.position = {
+          x: x + this.distance * Math.cos(this.angle),
+          y: y + this.distance * Math.sin(this.angle),
+        };
+        this.draw();
+      };
+    }
+
+    function Emitter(x, y) {
+      this.position = { x: x, y: y };
+      this.radius = 170;
+      this.count = 1000;
+      this.particles = [];
+
+      for (let i = 0; i < this.count; i++) {
+        this.particles.push(
+          new Particle(this.position.x, this.position.y, this.radius)
+        );
+      }
+    }
+
+    Emitter.prototype = {
+      draw: function () {
+        ctx.fillStyle = "rgba(0,0,0,1)";
+        ctx.beginPath();
+        ctx.arc(
+          this.position.x,
+          this.position.y,
+          this.radius,
+          0,
+          Math.PI * 2,
+          false
+        );
+        ctx.fill();
+        ctx.closePath();
+      },
+      update: function () {
+        for (let i = 0; i < this.count; i++) {
+          this.particles[i].update();
+        }
+        this.draw();
+      },
+    };
+
+    const emitter = new Emitter(canvas.width / 2, canvas.height / 2);
+
+    function loop() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      emitter.update();
+      window.requestAnimFrame(loop);
+    }
+
+    loop();
+
+    // Cleanup on unmount
+    return () => {
+      window.cancelAnimationFrame(loop);
+    };
+  }, []);
+
+  return <canvas ref={canvasRef} id="particle" />;
+};
+
+export default Starfield;
