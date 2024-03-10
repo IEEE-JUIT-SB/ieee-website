b507a672c79b63c8897bd234e8a8e4731763a8a6
Viren Tanti <Virentanti16@gmail.com>
2024-03-10 14:30:03 +0530

    hgg

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import Landing from "./components/landing/Landing";
 import About from "./components/About/About";
-import UpcomingEvents from "./components/upcomingEvents/UpcomingEvent";
+import UpcomingEvents from "./components/UpcomingEvents/UpcomingEvent";
 import Testimonials from "./components/testimonials/Testimonials";
-import ContactUs from "./components/contactUs/ContactUs";
-import Footer from "./components/footer/Footer";
+import ContactUs from "./components/ContactUs/ContactUs";
+import Footer from "./components/Footer/Footer";
 
 export default function Homepage() {
   return (
