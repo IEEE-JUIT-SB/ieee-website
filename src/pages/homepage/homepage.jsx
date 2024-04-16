ed80bc302f286a27ae1324ebd46607e97afc272b
Mayank Kumar <mayank.k9802@gmail.com>
2024-04-16 16:08:37 +0530

    Update 1

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import Landing from "./components/landing/Landing";
-import About from "./components/About/About";
-import UpcomingEvents from "./components/UpcomingEvents/UpcomingEvent";
+import About from "./components/about/About";
+import UpcomingEvents from "./components/upcomingEvents/UpcomingEvent";
 import TSlider from "./components/testimonials/TSlider";
-import ContactUs from "./components/ContactUs/ContactUs";
-import Footer from "./components/Footer/Footer";
+import ContactUs from "./components/contactUs/ContactUs";
+import Footer from "./components/footer/Footer";
 
 export default function Homepage() {
   return (
