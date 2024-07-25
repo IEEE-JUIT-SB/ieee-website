21d7b5089594027f96eb7890adc4588dba658f86
IEEE JUIT SB <ieeejuitsb@gmail.com>
2024-07-25 11:13:18 +0530

    Update homepage.jsx

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -4,7 +4,7 @@ import Landing from "./components/landing/Landing";
 import About from "./components/about/About";
 import UpcomingEvents from "./components/upcomingEvents/UpcomingEvent";
 import ContactUs from "./components/contactUs/ContactUs";
-import Footer from "./components/footer/Footer";
+import Footer from "./components/Footer/Footer";
 import Testimonials from "./components/testimonials/Testimonials";
 
 export default function Homepage() {
