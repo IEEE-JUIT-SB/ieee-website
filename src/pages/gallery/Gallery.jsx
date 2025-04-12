0dbb28d97c884245e08252ac7f0da8b13c86f1eb
jayendra-singh-k98 <jay290120@gmail.com>
2025-04-12 18:59:00 +0530

    add cards at gallery page

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,47 +1,66 @@
-import NoPage from "../noPage/NoPage";
-import "./Gallery.css";
-import image_1 from "./assets/image_1.jpg";
-import image_2 from "./assets/image_2.jpeg";
-import image_3 from "./assets/image_3.png";
-import image_4 from "./assets/image_4.png";
-import image_5 from "./assets/image_5.png";
-import image_6 from "./assets/image_6.jpg";
-import image_7 from "./assets/image_7.jpeg";
-import image_8 from "./assets/image_8.png";
-import image_9 from "./assets/image_9.png";
-import image_10 from "./assets/image_10.png";
+import React from "react";
+import './Gallery.css';
+import Navbar from "../homepage/components/navbar/Navbar";
+import Footer from "../homepage/components/Footer/Footer";
+import image_1 from './assets/image_1.jpg';
+import image_2 from './assets/image_2.png';
+import image_3 from './assets/image_3.png';
+import image_4 from './assets/image_4.png';
+import podcast from './assets/podcast.png';
 
-
-
-
+const cardsDetails = [
+    {
+        image: image_4,
+        date: "08 DEC 2024",
+        title: "CRYPTIC QUEST",
+        link: "/crypticquest",
+    },
+    {
+        image: image_2,
+        date: "06 DEC 2024",
+        title: "MURDER MYSTERY",
+        link: "/crypticquest",
+    },
+    {
+        image: image_3,
+        date: "08 DEC 2024",
+        title: "CODE WITH TRUST",
+        link: "/crypticquest",
+    },
+    {
+        image: image_1,
+        date: "07 DEC 2024",
+        title: "DR. TANU JAIN",
+        link: "/crypticquest",
+    },
+    {
+        image: podcast,
+        date: "07 DEC 2024",
+        title: "PODCAST",
+        link: "/crypticquest",
+    },
+];
 export default function Gallery() {
-  return (
-    <div className="gallery-page">
-      <NoPage />
-      <div className="banner">
-        <div className="slider" style={{ "--quantity": 10 }}>
-          <div className="item" style={{ "--position": 1 }}><a href="https://drive.google.com/drive/folders/1Fx8oJWcUE1vrbNMHsGI51obQSJQ3vhCy"><img src={image_1} alt="Gallery Image 1" /><div class="heading-div"><h2>Murder Mystry</h2></div></a></div>
-
-          <div className="item" style={{ "--position": 2 }}><a href="https://drive.google.com/drive/folders/1Fx8oJWcUE1vrbNMHsGI51obQSJQ3vhCy"><img src={image_2} alt="Gallery Image 2" /><div class="heading-div"><h2>Murder Mystry</h2></div></a></div>
-
-          <div className="item" style={{ "--position": 3 }}><a href="https://drive.google.com/drive/folders/1Fx8oJWcUE1vrbNMHsGI51obQSJQ3vhCy"><img src={image_3} alt="Gallery Image 3" /><div class="heading-div"><h2>Murder Mystry</h2></div></a></div>
-
-          <div className="item" style={{ "--position": 4 }}><a href="https://drive.google.com/drive/folders/1Fx8oJWcUE1vrbNMHsGI51obQSJQ3vhCy"><img src={image_4} alt="Gallery Image 4" /><div class="heading-div"><h2>Murder Mystry</h2></div></a></div>
-
-          <div className="item" style={{ "--position": 5 }}><a href="https://drive.google.com/drive/folders/1Fx8oJWcUE1vrbNMHsGI51obQSJQ3vhCy"><img src={image_5} alt="Gallery Image 5" /><div class="heading-div"><h2>Murder Mystry</h2></div></a></div>
-
-          <div className="item" style={{ "--position": 6 }}><a href="https://drive.google.com/drive/folders/1Fx8oJWcUE1vrbNMHsGI51obQSJQ3vhCy"><img src={image_6} alt="Gallery Image 6" /><div class="heading-div"><h2>Murder Mystry</h2></div></a></div>
-
-          <div className="item" style={{ "--position": 7 }}><a href="https://drive.google.com/drive/folders/1Fx8oJWcUE1vrbNMHsGI51obQSJQ3vhCy"><img src={image_7} alt="Gallery Image 7" /><div class="heading-div"><h2>Murder Mystry</h2></div></a></div>
-
-          <div className="item" style={{ "--position": 8 }}><a href="https://drive.google.com/drive/folders/1Fx8oJWcUE1vrbNMHsGI51obQSJQ3vhCy"><img src={image_8} alt="Gallery Image 8" /><div class="heading-div"><h2>Murder Mystry</h2></div></a></div>
-
-          <div className="item" style={{ "--position": 9 }}><a href="https://drive.google.com/drive/folders/1Fx8oJWcUE1vrbNMHsGI51obQSJQ3vhCy"><img src={image_9} alt="Gallery Image 9" /><div class="heading-div"><h2>Murder Mystry</h2></div></a></div>
-
-          <div className="item" style={{ "--position": 10 }}><a href="https://drive.google.com/drive/folders/1Fx8oJWcUE1vrbNMHsGI51obQSJQ3vhCy"><img src={image_10} alt="Gallery Image 10" /><div class="heading-div"><h2>Murder Mystry</h2></div></a></div>
+    return (
+        <div className="gallery-container">
+            <Navbar />
+            <h1>Events 2024-25</h1>
+            <div className="cards-container">
+                {cardsDetails.map((card, index) => (
+                    <a href={card.link} key={index} >
+                        <div className="card">
+                            <div className="image">
+                                <img src={card.image} alt={card.title} />
+                            </div>
+                            <div className="lowerContent">
+                                <div className="post-date">{card.date}</div>
+                                <h3>{card.title}</h3>
+                            </div>
+                        </div>
+                    </a>
+                ))}
+            </div>
+            <Footer />
         </div>
-      </div>
-    </div>
-  );
+    );
 }
-
--- src/pages/noPage/NoPage.jsx
+++ src/pages/noPage/NoPage.jsx
@@ -4,15 +4,17 @@ import Footer from "../homepage/components/Footer/Footer";
 import Asset2 from "../homepage/components/landing/Assets/asset2.png";
 import Navbar from "../homepage/components/navbar/Navbar";
 import Starfield from "../homepage/components/landing/Starfield";
+import Gallery from "../gallery/Gallery"
 
 export default function NoPage() {
   return (
     <div className="no-page">
       <Navbar />
+      <Gallery/>
       <div className="">
-        <img src={Asset2} alt="" id="bg-asset" className="nopage-bg" />
+        {/* <img src={Asset2} alt="" id="bg-asset" className="nopage-bg" /> */}
         <div className="starfield-nopage">
-          <Starfield />
+          {/* <Starfield /> */}
         </div>
         <div className="no-page-in-1">
           {/* <h1>IEEE</h1> */}
